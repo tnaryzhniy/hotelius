357d84624a93aa3534b110cf69b636ce6b2b02c1
Taras <tariq@qip.ru>
2015-05-26 18:38:49 +0300

    Finish model tests. Upgrade partials and views for #index and #show actions

--- копия - app - копия/controllers/landings_controller.rb
+++
@@ -1,5 +0,0 @@
-class LandingsController < ApplicationController
-  def index
-    @top5 = Hotel.top_five
-  end
-end
--- копия - app - копия/models/raiting.rb
+++
@@ -1,6 +0,0 @@
-class Raiting < ActiveRecord::Base
-  belongs_to :hotel
-  belongs_to :user
-
-  validates :raiting, presence: true
-end
